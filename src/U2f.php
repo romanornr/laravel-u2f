999b62fae0014404bce01f14a1074e2780d2dc48
romanornr <romanornr@gmail.com>
2017-03-12 19:29:13 +0100

    Session set to put

--- src/U2f.php
+++ src/U2f.php
@@ -112,7 +112,7 @@ class U2f
         $U2fKey->counter = $reg->counter;
         $U2fKey->save();
 
-        $this->session->set($this->config->get('u2f.sessionU2fName'), true);
+        $this->session->put($this->config->get('u2f.sessionU2fName'), true);
 
         return $U2fKey;
     }
